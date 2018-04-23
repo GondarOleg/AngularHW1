c2d5222ce38eeebf411e5240bbd6bcb8fb3e5d02
Гондарь Олег Николаевич <oleh.hondar@a-bank.com.ua>
2018-04-23 11:43:36 +0300

    homework1

--- task1.js
+++ task1.js
@@ -6,15 +6,14 @@ feedbacks = [
 {'id': 1027, 'user_id': 22341, 'msg': 'Comment 5', 'rate': 3 },
 {'id': 1028, 'user_id': 75874, 'msg': 'Comment 6', 'rate': 5 },
 {'id': 1029, 'user_id': 11223, 'msg': 'Comment 7', 'rate': 1 },
-{'id': 1030, 'user_id': 44421, 'msg': 'Comment 8', 'rate': 4 },
-{'id': 1024, 'user_id': 44393, 'msg': 'Comment 1', 'rate': 5 }
+{'id': 1030, 'user_id': 44421, 'msg': 'Comment 8', 'rate': 4 }
  ];
 
 var occ = 0;
 var id;
 
 for(let feedback of feedbacks){
-		var elOcc = feedbacks.filter(x => x.id === feedback.id).length;
+		var elOcc = feedbacks.filter(x => x.user_id === feedback.user_id).length;
 	if( elOcc > occ){
 		occ = elOcc;
 		id = feedback.id;
